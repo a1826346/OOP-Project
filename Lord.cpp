75b3f268e79b3e33018360559c2ee341964a96f5
a1826346 <a1826346@adelaide.edu.au>
2022-05-24 13:08:26 +0100

    added action text

--- Lord.cpp
+++ Lord.cpp
@@ -9,7 +9,7 @@ Lord::Lord(){
 void Lord::generateMoves(){
     Move deathRune;
     deathRune.setMoveName("Rune of Death");
-    deathRune.setActionText("...");
+    deathRune.setActionText("A wave of malevolence washes over you.");
     deathRune.setMagicCost(0);
     deathRune.setDamage(30);
     deathRune.setHitChance(40);
@@ -20,7 +20,7 @@ void Lord::generateMoves(){
 
     Move daggerRain;
     daggerRain.setMoveName("Dagger Rain");
-    daggerRain.setActionText("...");
+    daggerRain.setActionText("Daggers fall from above as if like rain.");
     daggerRain.setMagicCost(0);
     daggerRain.setDamage(15);
     daggerRain.setHitChance(80);
@@ -31,7 +31,7 @@ void Lord::generateMoves(){
 
     Move maceSwing;
     maceSwing.setMoveName("Mace Swing");
-    maceSwing.setActionText("...");
+    maceSwing.setActionText("Drace wildly swings his mace toward you.");
     maceSwing.setMagicCost(0);
     maceSwing.setDamage(10);
     maceSwing.setHitChance(100);
@@ -42,7 +42,7 @@ void Lord::generateMoves(){
 
     Move regenerate;
     regenerate.setMoveName("Regenerate Health");
-    regenerate.setActionText("...");
+    regenerate.setActionText("Drace appears to grow stronger before your very eyes.");
     regenerate.setMagicCost(0);
     regenerate.setDamage(0);
     regenerate.setHitChance(75);
